4bb15309222fe54999ac59abb92f3f8ecb2cda7d
Polina <mshow7830@gmail.com>
2026-07-13 09:32:49 +0300

    feat: slices 5-remove el

--- 02-maps/main.go
+++ 02-maps/main.go
@@ -1,1 +1,22 @@
 package main
+import(
+	"fmt"
+	"errors"
+)
+
+func main(){
+	res, err := sum(4,-5)
+	if err != nil{
+		fmt.Print("oshibka", err)
+	} else{
+		fmt.Print(res)
+	}
+}
+func sum(a, b int)(int, error){
+	if a>0 && b>0{
+		return a+b, nil
+	} else{
+		return 0, errors.New("ыыыы ошиибка")
+	}
+	
+}
